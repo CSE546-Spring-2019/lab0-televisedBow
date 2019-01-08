7a199a64d1fc2c87efee1158a382e868054817f4
Gabe Sellers <sellers.gabe@gmail.com>
2019-01-08 11:32:28 -0500

    Added comments

---
+++ README.md
@@ -0,0 +1,6 @@
+Lab0 by Gabe Sellers
+
+To compile: run `make`
+To run: `./hello`
+
+Any question please email sellers.187@osu.edu
--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
+/*
+ * Lab 0
+ * CSE 5462
+ * By: Gabe Sellers
+ */ 
+
 #include <stdio.h>
-int main()
-{
-  printf ("Hello Gabe Sellers!\n");
+int main(){
+  printf ("Hello World, I'm Gabe Sellers!\n");
   return 0;
-  }
+ }
